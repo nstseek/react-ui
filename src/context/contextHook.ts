59e88a00c5f339c6bed8920c24295208030253ab
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-01-20 18:26:19 -0300

    Adds string generator for modal id

--- src/context/contextHook.ts
+++ src/context/contextHook.ts
@@ -4,21 +4,34 @@ export interface ModalData {
   title: string;
   desc: string;
   type: 'ok' | 'warning' | 'error';
-  id: string;
+  id?: string;
+}
+
+export interface ModalModel extends ModalData {
+  id: ModalData['id'];
 }
 
 export interface ContextState {
   loading: boolean[];
-  modal: ModalData[];
+  modal: ModalModel[];
   addModal(modal: ModalData);
   removeModal(id: ModalData['id']);
   pushLoading();
   popLoading();
 }
 
+const generateStr = (length: number) => {
+  let tmpStr = '';
+  for (let i = 0; i < length; i++) {
+    tmpStr += String.fromCharCode(33 + Number((Math.random() * 93).toFixed(0)));
+  }
+  return tmpStr;
+};
+
 export const useRootContext = () => {
   const addModal = (modal: ModalData) => {
-    const modalArr = [...ctx.modal, modal];
+    const tmpModal = { ...modal, id: modal.id ? modal.id : generateStr(9) };
+    const modalArr = [...ctx.modal, tmpModal];
     setCtx({ ...ctx, modal: modalArr });
   };
 
